1944d113e9988f5d50f126e96b3f86ccfa84b9a9
tartinax <pokemon.furtif@gmail.com>
2025-11-13 23:11:24 +0100

    Add error handling to snapshot and instruction logic
    Wrapped switch statements in try-catch blocks in SnapshotTracker, SnapshotFactory, and InstructionManager to improve error handling and prevent crashes from unexpected exceptions. Error messages are now printed to the console instead of stack traces, making debugging easier and reducing noisy output.

--- shared/src/main/java/io/github/shared/local/shared_engine/manager/InstructionManager.java
+++ shared/src/main/java/io/github/shared/local/shared_engine/manager/InstructionManager.java
@@ -23,68 +23,73 @@ import io.github.shared.local.shared_engine.factory.EntityFactory;
 public class InstructionManager {
     public static void executeInstruction(Instruction instruction, IGame game){
         String type = instruction.getClass().getSimpleName();
-        switch (type) {
-            case "CreateInstruction":
-                CreateInstruction ci = (CreateInstruction) instruction;
+        try {
+            switch (type) {
+                case "CreateInstruction":
+                    CreateInstruction ci = (CreateInstruction) instruction;
 
-                ComponentMapper<NetComponent> netMapper = game.getWorld().getMapper(NetComponent.class);
-                ComponentMapper<OnCreationComponent> onCreateMapper = game.getWorld().getMapper(OnCreationComponent.class);
-                ComponentMapper<ProprietyComponent> proprietyMapper = game.getWorld().getMapper(ProprietyComponent.class);
-                for (int i = 0; i < ci.getToSpawn().size(); i++) {
-                    int  x = ci.getPosX().get(i);
-                    int y = ci.getPosY().get(i);
-                    int netId = ci.getNetId().get(i);
-                    int from = ci.getFrom().get(i);
-                    EntityType entityType = ci.getToSpawn().get(i);
-                    Entity entity = game.getWorld().createEntity();
+                    ComponentMapper<NetComponent> netMapper = game.getWorld().getMapper(NetComponent.class);
+                    ComponentMapper<OnCreationComponent> onCreateMapper = game.getWorld().getMapper(OnCreationComponent.class);
+                    ComponentMapper<ProprietyComponent> proprietyMapper = game.getWorld().getMapper(ProprietyComponent.class);
+                    for (int i = 0; i < ci.getToSpawn().size(); i++) {
+                        int x = ci.getPosX().get(i);
+                        int y = ci.getPosY().get(i);
+                        int netId = ci.getNetId().get(i);
+                        int from = ci.getFrom().get(i);
+                        EntityType entityType = ci.getToSpawn().get(i);
+                        Entity entity = game.getWorld().createEntity();
 
-                    Player player = Utility.findPlayerByUuid(game.getPlayersList(), ci.getPlayer().get(i));
-                    if(player !=null){
-                        Utility.subtractResourcesInPlace(player.getResources(),entityType.getCost());
-                        ProprietyComponent prc = proprietyMapper.create(entity);
-                        prc.set(player.getUuid(), Utility.findTeamByPlayer(player,game.getPlayerTeam()));
+                        Player player = Utility.findPlayerByUuid(game.getPlayersList(), ci.getPlayer().get(i));
+                        if (player != null) {
+                            Utility.subtractResourcesInPlace(player.getResources(), entityType.getCost());
+                            ProprietyComponent prc = proprietyMapper.create(entity);
+                            prc.set(player.getUuid(), Utility.findTeamByPlayer(player, game.getPlayerTeam()));
+                        }
+
+                        if (entityType.getType().equals(EntityType.Type.Building)) {
+                            ShapeManager.overlayShape(game.getMap(), new Shape(entityType.getShapeType().getShape(), netId), x, y);
+                        }
+
+
+                        NetComponent nc = netMapper.create(entity);
+                        nc.set(netId, entityType);
+
+                        OnCreationComponent occ = onCreateMapper.create(entity);
+                        occ.set(x, y, from, entityType.getCreate_time());
                     }
+                    break;
 
-                    if(entityType.getType().equals(EntityType.Type.Building)){
-                        ShapeManager.overlayShape(game.getMap(),new Shape(entityType.getShapeType().getShape(),netId),x,y);
+                case "UpdateEntityInstruction":
+                    for (EntitySnapshot snapshot : ((UpdateEntityInstruction) (instruction)).getToUpdate()) {
+                        EntityFactory.applySnapshotToEntity(game.getWorld(), snapshot);
                     }
-
-
-                    NetComponent nc = netMapper.create(entity);
-                    nc.set(netId, entityType);
-
-                    OnCreationComponent occ = onCreateMapper.create(entity);
-                    occ.set(x, y, from,entityType.getCreate_time());
-                }
-                break;
-
-            case "UpdateEntityInstruction":
-                for (EntitySnapshot snapshot : ((UpdateEntityInstruction)(instruction)).getToUpdate()){
-                    EntityFactory.applySnapshotToEntity(game.getWorld(),snapshot);
-                }
-                break;
-            case "DestroyInstruction":
-                for (int netId : ((DestroyInstruction)(instruction)).getToKill()){
-                    EntityFactory.destroyEntityByNetId(game.getWorld(),netId);
-                }
-                break;
-            case "ResourcesInstruction":
-                ResourcesInstruction resourcesInstruction = (ResourcesInstruction) instruction;
-                for (Player player : game.getPlayersList()){
-                    if(player.getUuid().equals(resourcesInstruction.getPlayer())){
-                        player.setResources(resourcesInstruction.getRessources());
+                    break;
+                case "DestroyInstruction":
+                    for (int netId : ((DestroyInstruction) (instruction)).getToKill()) {
+                        EntityFactory.destroyEntityByNetId(game.getWorld(), netId);
                     }
-                }
-                break;
-            case "EventsInstruction":
-                game.setCurrentEvent(((EventsInstruction)(instruction)).getEventType());
-                break;
-            case "FinalInstruction":
-                break;
-            case "SpecialRequestsInstruction":
-                break;
-            default:
-                throw new IllegalArgumentException("Instruction non pris en charge : " + type);
+                    break;
+                case "ResourcesInstruction":
+                    ResourcesInstruction resourcesInstruction = (ResourcesInstruction) instruction;
+                    for (Player player : game.getPlayersList()) {
+                        if (player.getUuid().equals(resourcesInstruction.getPlayer())) {
+                            player.setResources(resourcesInstruction.getRessources());
+                        }
+                    }
+                    break;
+                case "EventsInstruction":
+                    game.setCurrentEvent(((EventsInstruction) (instruction)).getEventType());
+                    break;
+                case "FinalInstruction":
+                    break;
+                case "SpecialRequestsInstruction":
+                    break;
+                default:
+                    throw new IllegalArgumentException("Instruction non pris en charge : " + type);
+            }
+        }
+        catch (Exception e){
+            System.err.print("executeInstruction err "+e);
         }
     }
 }
